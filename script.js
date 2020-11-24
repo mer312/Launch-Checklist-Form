1dc678e396d3673341835b9c45ed5c46a156e583
Matt R <mattreimer1@yahoo.com>
2020-11-24 14:57:56 -0600

    second commit ass5

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-// Write your JavaScript code here!
+window.addEventListener('load', function() {
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+   let pilotName = document.getElementById("pilotName");
+   let copilotName = document.getElementById("copilotName");
+   let fuelLevel = document.getElementById("fuelLevel");
+   let cargoMass = document.getElementById("cargoMass");
+   let form1 = document.getElementById("launchForm");
+
+   form1.addEventListener("submit", function(event){
+   if(pilotName !== "Chris"){
+   event.preventDefault();
+
+}
+   if(copilotName !== "Blake"){
+   event.preventDefault();
+}
+   if(!fuelLevel){
+   event.preventDefault();
+}
+   if(!cargoMass){
+   event.preventDefault();
+}
+
+   })
+
+   <h2>Mission Destination</h2>
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then( function(json) {
+      const div = document.getElementById("missionTarget");
+         div.innerHTML = `
+   <ol>
+      <li>Name: ${json.name}</li>
+      <li>Diameter: ${json.diameter}</li>
+      <li>Star: ${json.star}</li>
+      <li>Distance from Earth: ${json.distance}</li>
+      <li>Number of Moons: ${json.moons}</li>
+   </ol>
+<img src="${json.image}">
+            `;
+         });
+      });
+   })
+
+// // This block of code shows how to format the HTML once you fetch some plan
